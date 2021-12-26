129b67394d075704c123edaafa5f3f55bdc3a2f3
masamiti <azain@gmail.com>
2021-12-26 18:06:04 +0900

    setting

--- short_url/settings.py
+++ short_url/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from socket import gethostname
-import os, environ, djang_heroku
+import os, environ, django_heroku
 
 hostname = gethostname()
 
